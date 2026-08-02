49c0f9eaabacef9f733c234e0d9b077357e76e0f
Uzair-Debugger <smuzair14cse@gmail.com>
2026-08-03 00:54:50 +0500

    feat: add GET and POST endpoints for settings management

---
+++ src/app/api/settings/get/route.ts
@@ -0,0 +1,27 @@
+import { NextRequest, NextResponse } from "next/server";
+import { prisma } from "@/lib/prisma";
+
+export async function GET(req:NextRequest) {
+
+    try {
+        const { ownerId } = await req.json()
+            if (!ownerId) {
+              return NextResponse.json(
+                { message: "Owner id is required" },
+                { status: 400 }
+              );
+            }
+        
+            const setting = await prisma.settings.findFirst(                {
+                    where:{
+                        ownerId
+                    }
+                })
+        
+            return NextResponse.json(setting, { status: 200 });
+    } catch (error) {
+        console.error(error);
+        return NextResponse.json({ message: "Internal Server Error" },{ status: 500 }
+);
+    }
+}
